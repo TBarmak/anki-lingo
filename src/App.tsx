0563990993817c57304876596aa4d81d4e0d3d96
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-01-21 19:25:27 -0500

    Set up proxy for api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
+  }, []);
+
   return (
-    <div className='w-screen h-screen bg-blue-800 flex flex-col justify-center items-center'>
-      <h1 className='text-white text-4xl'>Anki Lang</h1>
+    <div className="w-screen h-screen bg-blue-800 flex flex-col justify-center items-center">
+      <h1 className="text-white text-4xl">Anki Lang</h1>
+      <p className="text-white my-2">The current time is {currentTime}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
