d1f45354d5e8d91e61faa73772f35ca11613e588
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-02-08 23:12:58 -0500

    Add vitest

---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App.tsx', async () => {
+    it('Then should show header with "Anki Lingo"', async () => {
+        // Act
+        render(<App />);
+
+        // Assert
+        const title = screen.queryByText('Anki Lingo');
+        expect(title).not.toBeNull();
+    });
+});
