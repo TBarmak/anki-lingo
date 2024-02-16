b0a66f89809eb64a5c7db7281133b437ac876311
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-02-16 17:24:25 -0500

    Save progress

--- api/api.py
+++ api/api.py
@@ -9,104 +9,110 @@ import hashlib
 import os
 
 FLASHCARD_FIELDS = {
-	"inputWord": "input word",
-	"word": "word",
-	"pos": "part of speech",
-	"definition": "definition",
-	"translations": "translations",
-	"targetExampleSentences": "target example sentences",
-	"nativeExampleSentences": "native example sentences",
-	"audioFilenames": "audio"
+    "inputWord": "input word",
+    "word": "word",
+    "pos": "part of speech",
+    "definition": "definition",
+    "translations": "translations",
+    "targetExampleSentences": "target example sentences",
+    "nativeExampleSentences": "native example sentences",
+    "audioFilenames": "audio"
 }
 
 LANGUAGE_RESOURCES = [
-	{
-		"name": "Word Reference",
-		"route": "api/wr/",
-		"args": ["targetLang", "nativeLang", "word"],
-		"outputs": ["word", "pos", "definition", "translations","targetExampleSentences", "nativeExampleSentences"],
-		"supportedLanguages": ["english", "español", "português", "français"]
-	},
-	{
-		"name": "SpanishDict",
-		"route": "api/spanishdict/",
-		"args": ["targetLang", "word"],
-		"outputs": ["word", "pos", "translations", "targetExampleSentences", "nativeExampleSentences"],
-		"supportedLanguages": ["english", "español"]
-	},
-	{
-		"name": "Michaelis BR",
-		"route": "api/michaelis-br/",
-		"args": ["word"],
-		"outputs": ["word", "pos", "definition", "targetExampleSentences"],
-		"supportedLanguages": ["português"]
-	},
-	{
-		"name": "Forvo",
-		"route": "api/forvo/",
-		"args": ["targetLang", "word"],
-		"outputs": ["audioFilenames"],
-		"supportedLanguages": ["français", "português", "español", "english"]	
-	}
+    {
+        "name": "Word Reference",
+        "route": "api/wr/",
+                "args": ["targetLang", "nativeLang", "word"],
+                "outputs": ["word", "pos", "definition", "translations", "targetExampleSentences", "nativeExampleSentences"],
+                "supportedLanguages": ["english", "español", "português", "français"]
+    },
+    {
+        "name": "SpanishDict",
+        "route": "api/spanishdict/",
+                "args": ["targetLang", "word"],
+                "outputs": ["word", "pos", "translations", "targetExampleSentences", "nativeExampleSentences"],
+                "supportedLanguages": ["english", "español"]
+    },
+    {
+        "name": "Michaelis BR",
+        "route": "api/michaelis-br/",
+                "args": ["word"],
+                "outputs": ["word", "pos", "definition", "targetExampleSentences"],
+                "supportedLanguages": ["português"]
+    },
+    {
+        "name": "Forvo",
+        "route": "api/forvo/",
+                "args": ["targetLang", "word"],
+                "outputs": ["audioFilenames"],
+                "supportedLanguages": ["français", "português", "español", "english"]
+    }
 ]
 
+
 def create_app():
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	@app.route('/api/supported-languages')
-	def get_languages():
-		supported_languages = [language.capitalize() for resource in LANGUAGE_RESOURCES for language in resource['supportedLanguages']]
-		return {'languages': sorted(list(set(supported_languages)))}
+    @app.route('/api/supported-languages')
+    def get_languages():
+        supported_languages = [language.capitalize(
+        ) for resource in LANGUAGE_RESOURCES for language in resource['supportedLanguages']]
+        return {'languages': sorted(list(set(supported_languages)))}
 
-	@app.route('/api/resources/<language>')
-	def get_resources(language):
-		resources = filter(lambda x: language.lower() in x['supportedLanguages'], LANGUAGE_RESOURCES)
-		return {'resources': list(resources)}
+    @app.route('/api/resources/<language>')
+    def get_resources(language):
+        resources = filter(lambda x: language.lower()
+                           in x['supportedLanguages'], LANGUAGE_RESOURCES)
+        return {'resources': list(resources)}
 
-	@app.route('/api/field-mapping')
-	def get_field_mapping():
-		return FLASHCARD_FIELDS
+    @app.route('/api/field-mapping')
+    def get_field_mapping():
+        return FLASHCARD_FIELDS
 
-	@app.route('/api/wr/<target_lang>/<native_lang>/<word>')
-	def get_wr_word(target_lang, native_lang, word):
-		return {'inputWord': word, 'scrapedWordData': scrape_word_reference(word, target_lang, native_lang)}
+    @app.route('/api/wr/<target_lang>/<native_lang>/<word>')
+    def get_wr_word(target_lang, native_lang, word):
+        return {'inputWord': word, 'scrapedWordData': scrape_word_reference(word, target_lang, native_lang)}
 
-	@app.route('/api/spanishdict/<target_lang>/<word>')
-	def get_spanishdict_word(target_lang, word):
-		return {'inputWord': word, 'scrapedWordData': scrape_spanishdict(word, target_lang)}
+    @app.route('/api/spanishdict/<target_lang>/<word>')
+    def get_spanishdict_word(target_lang, word):
+        return {'inputWord': word, 'scrapedWordData': scrape_spanishdict(word, target_lang)}
 
-	@app.route('/api/michaelis-br/<word>')
-	def get_michaelis_br_word(word):
-		return {'inputWord': word, 'scrapedWordData': scrape_michaelis(word)}
+    @app.route('/api/michaelis-br/<word>')
+    def get_michaelis_br_word(word):
+        return {'inputWord': word, 'scrapedWordData': scrape_michaelis(word)}
 
-	@app.route('/api/forvo/<target_lang>/<word>')
-	def get_forvo_audio(target_lang, word):
-		return {'inputWord': word, 'scrapedWordData': scrape_forvo(word, target_lang)}
+    @app.route('/api/forvo/<target_lang>/<word>')
+    def get_forvo_audio(target_lang, word):
+        return {'inputWord': word, 'scrapedWordData': scrape_forvo(word, target_lang)}
 
-	@app.route('/api/format-csv', methods=['POST'])
-	def format_csv():
-		req = request.json
-		hash = hashlib.sha256(str(req).encode()).hexdigest()
-		zip_filename = f'anki-{hash}.zip'
-		audio_filepath = 'audio_files/'
-		output_filepath = os.getcwd() + '/zip_files/'
-		rows = []
-		for word_data in req['scrapedData']:
-			audio_filenames = [filename for item in word_data['scrapedWordData'] for filename in item.get('audioFilenames', [])]
-			with ZipFile(output_filepath + zip_filename, 'a') as zip_object:
-				for audio_filename in audio_filenames:
-					zip_object.write(audio_filepath + audio_filename, arcname='audio_files/' + audio_filename)
-			card_sides = create_csv_sides(word_data, req['cardFormat'])
-			row = '|'.join(card_sides)
-			rows.append(row)
-		csv = '\n'.join(rows)
-		with ZipFile(output_filepath + zip_filename, 'a') as zip_object:
-			zip_object.writestr('anki.csv', csv)
-		return send_file(output_filepath + zip_filename)
-	
-	return app
+    @app.route('/api/format-csv', methods=['POST'])
+    def format_csv():
+        req = request.json
+        hash = hashlib.sha256(str(req).encode()).hexdigest()
+        zip_filename = f'anki-{hash}.zip'
+        audio_filepath = 'audio_files/'
+        output_filepath = os.getcwd() + '/zip_files/'
+        rows = []
+        for word_data in req['scrapedData']:
+            audio_filenames = [filename for item in word_data['scrapedWordData']
+                               for filename in item.get('audioFilenames', [])]
+            with ZipFile(output_filepath + zip_filename, 'a') as zip_object:
+                for audio_filename in audio_filenames:
+                    zip_object.write(audio_filepath + audio_filename,
+                                     arcname='audio_files/' + audio_filename)
+            card_sides = create_csv_sides(word_data, req['cardFormat'])
+            row = '|'.join(card_sides)
+            rows.append(row)
+        csv = '\n'.join(rows)
+        with ZipFile(output_filepath + zip_filename, 'a') as zip_object:
+            zip_object.writestr('anki.csv', csv)
+        return send_file(output_filepath + zip_filename)
+
+    return app
+
 
 app = create_app()
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
--- api/utils/format_csv.py
+++ api/utils/format_csv.py
@@ -16,17 +16,58 @@ def create_csv_sides(word_data, card_format):
     return [front] + sides
 
 
+def restructure_scraped_dict(scraped_word_data, fields):
+    '''
+    Restructures the list of entries to be grouped by word and/or part of speech, depending on which are included in the fields
+
+    Parameters
+    ------------
+
+    Return
+    ------------
+    '''
+    restructure_key = [key for key in ['word', 'pos'] if key in fields]
+    restructured_dict = {}
+    for item in scraped_word_data:
+        item_key = " ".join([item.get(key, "")
+                            for key in restructure_key]).strip()
+        if restructured_dict.get(item_key):
+            restructured_dict[item_key].append(item)
+        else:
+            restructured_dict[item_key] = [item]
+    return restructured_dict
+
+
 def create_csv_side(scraped_word_data, side_format):
-    formatted_entries = [format_entry(entry, side_format)
-                         for entry in scraped_word_data]
-    separator = "<br><br>" if side_format['useWhitespace'] else "<br>"
-    return separator.join(formatted_entries)
+    '''
+    Formats the scraped data for one side of the flashcard based on the format
+
+    Parameters
+    ------------
+        scraped_word_data: list of scraped data (dicts)
+        side_format: 
+
+    Return
+    ------------
+
+    '''
+    # Check if word or pos in side_format
+    fields = side_format['fields']
+    restructured_dict = restructure_scraped_dict(scraped_word_data, fields)
+
+    blocks = []
+    for key in restructured_dict.keys():
+        formatted = [key] if key else []
+        formatted += [format_entry(entry, fields)
+                      for entry in restructured_dict[key]]
+        blocks.append('<br>'.join(formatted))
+    return '<br><br>'.join(blocks)
 
 
-def format_entry(entry, side_format):
+def format_entry(entry, fields):
     # Filter the entry to the fields that are in side_format and not null/empty
     filtered_entry = [[key, entry[key]]
-                      for key in side_format['fields'] if entry.get(key)]
+                      for key in fields if entry.get(key) and key not in ['word', 'pos']]
     for field in filtered_entry:
         if field[0] == 'audioFilenames':
             field[1] = [f"[sound:{filename}]" for filename in field[1]]
