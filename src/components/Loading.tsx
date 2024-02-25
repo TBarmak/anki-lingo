33d6e724edbf6b5e1f790012020cff3311d5d134
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-02-24 20:51:09 -0500

    Add loader spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,12 @@
+import { Bars } from "react-loader-spinner";
+
 export default function Loading() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <p className="text-xl secondary-text">Loading...</p>
+      <div className="my-4">
+        <Bars height="60" width="60" color="#162e50" />
+      </div>
+      <p className="text-xl secondary-text">Creating your flashcards...</p>
     </div>
   );
 }
