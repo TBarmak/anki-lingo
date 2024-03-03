e892905a1b298e77e1b99d5c3e460a8fd2c4ab0e
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-03-02 23:08:17 -0500

    Update test for App.spec.tsx

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,14 +1,23 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { BrowserRouter } from "react-router-dom";
 
 describe("App.tsx", async () => {
-  it('Then should show header with "Anki Lingo"', async () => {
+  it('Then should show "Anki Lingo" and blurb', async () => {
     // Act
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
 
     // Assert
-    const title = screen.queryByText("Anki Lingo");
+    const title = screen.queryAllByText("Anki Lingo");
+    const blurb = screen.queryByText(
+      "Automate generating anki flashcards for learning foreign languages."
+    );
     expect(title).not.toBeNull();
+    expect(blurb).not.toBeNull();
   });
 });
