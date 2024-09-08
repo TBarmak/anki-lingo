4a76bf250a8a9dd4f964bd767b36fb8f6ca35c25
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2024-09-08 00:08:10 -0400

    [Feat] Show health checks for scraper endpoints (#18)
    * feat: add health checks for endpoints
    
    * feat: show health checks on home page
    
    * UX improvements
    
    * Update images and remove comment

--- src/components/forms/ResourceForm.tsx
+++ src/components/forms/ResourceForm.tsx
@@ -25,9 +25,6 @@ export default function ResourceForm({
   setExportFields,
 }: Props) {
   const [supportedLanguages, setSupportedLanguages] = useState<string[]>([]);
-  const [languageResources, setLanguageResources] = useState<
-    LanguageResource[]
-  >([]);
   const [inputFields, setInputFields] = useState<InputFields>({
     words: "",
     targetLanguage: "",
@@ -37,7 +34,7 @@ export default function ResourceForm({
   const [errors, setErrors] = useState<FormErrors>({} as FormErrors);
 
   useEffect(() => {
-    fetch("/api/supported-languages")
+    fetch("api/supported-languages")
       .then((res) => res.json())
       .then((data) => setSupportedLanguages(data.languages));
   }, []);
@@ -45,17 +42,26 @@ export default function ResourceForm({
   useEffect(() => {
     const language = inputFields.targetLanguage;
     if (language) {
-      fetch(`/api/resources/${inputFields.targetLanguage}`)
+      fetch(`api/resources/${inputFields.targetLanguage}`)
         .then((res) => res.json())
-        .then((data) => setLanguageResources(data.resources));
+        .then(async (data) => {
+          const healthPromises: Promise<boolean>[] = data.resources.map(
+            (resource: LanguageResource) => {
+              return new Promise((res) => {
+                fetch(resource.healthRoute).then((response) => {
+                  resource["isHealthy"] = response.ok;
+                  res(true);
+                });
+              });
+            }
+          );
+          await Promise.all(healthPromises);
+          setInputFields({ ...inputFields, languageResources: data.resources });
+        });
     } else {
-      setLanguageResources([]);
+      setInputFields({ ...inputFields, languageResources: [] });
     }
   }, [inputFields.targetLanguage]);
-
-  useEffect(() => {
-    setInputFields({ ...inputFields, languageResources: languageResources });
-  }, [languageResources]);
 
   useEffect(() => {
     const exportFields = ([] as string[]).concat(
@@ -97,8 +103,11 @@ export default function ResourceForm({
       HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
     >
   ) {
+    // Clear the resources if the target language changed
     const newInputFields: InputFields = {
       ...inputFields,
+      languageResources:
+        e.target.name === "targetLanguage" ? [] : inputFields.languageResources,
       [e.target.name]: e.target.value,
     };
 
@@ -154,7 +163,7 @@ export default function ResourceForm({
 
   return (
     <motion.form
-      className="py-8 px-20 flex flex-col md:flex-row justify-between w-full min-h-full h-full"
+      className="py-8 px-16 md:px-24 flex flex-col md:flex-row justify-between w-full min-h-full"
       onSubmit={handleSubmit}
       variants={{
         exit: {
@@ -165,7 +174,7 @@ export default function ResourceForm({
       exit="exit"
     >
       <motion.div
-        className="flex-1 p-4"
+        className="flex-[3] mb-6 w-full"
         variants={{
           hidden: { opacity: 0, x: -100 },
           visible: { opacity: 1, x: 0 },
@@ -174,6 +183,7 @@ export default function ResourceForm({
         animate="visible"
         transition={{ duration: 0.75, delay: 0.5 }}
       >
+        <p className="font-bold secondary-text">Words/Phrases</p>
         <textarea
           name="words"
           className="w-full secondary-text h-full min-h-60 resize-none p-3 rounded-lg input text-lg"
@@ -185,8 +195,8 @@ export default function ResourceForm({
         />
         <FormError message={errors.words} />
       </motion.div>
-      <div className="h-full flex-1 p-4 flex flex-col items-center">
-        <div className="flex-1 flex flex-col items-center w-full px-12 md:px-24">
+      <div className="h-full w-full flex-[2] flex flex-col items-center md:pl-24">
+        <div className="flex-1 flex flex-col items-center w-full">
           <motion.div
             className="mb-4 flex flex-col w-full"
             variants={{
@@ -197,6 +207,7 @@ export default function ResourceForm({
             animate="visible"
             transition={{ duration: 0.75, delay: 0.5 }}
           >
+            <p className="font-bold secondary-text">Target language</p>
             <select
               name="targetLanguage"
               className="secondary-text p-2 pr-8 rounded-lg input text-lg"
@@ -224,6 +235,7 @@ export default function ResourceForm({
             animate="visible"
             transition={{ duration: 0.75, delay: 0.75 }}
           >
+            <p className="font-bold secondary-text">Native language</p>
             <select
               name="nativeLanguage"
               className="secondary-text p-2 pr-8 rounded-lg input text-lg"
@@ -246,7 +258,7 @@ export default function ResourceForm({
               inputFields.nativeLanguage &&
               inputFields.nativeLanguage !== inputFields.targetLanguage && (
                 <motion.div
-                  className="flex-1 flex flex-col w-full px-20 my-4"
+                  className="flex-1 flex flex-col w-full my-4"
                   variants={{
                     hidden: { opacity: 0, x: 100 },
                     visible: { opacity: 1, x: 0 },
@@ -296,10 +308,23 @@ export default function ResourceForm({
                           )}
                         </div>
                         {resource.name}
+                        <div
+                          className={`mx-2 w-4 h-4 rounded-full relative ${
+                            resource.isHealthy
+                              ? "green-background"
+                              : "accent-background"
+                          }`}
+                        >
+                          <div className="absolute z-50 opacity-0 hover:opacity-100 w-max bg-white px-2 p-1 rounded-lg text-sm -top-1 transition-all duration-300">
+                            {resource.isHealthy
+                              ? "Working as expected"
+                              : "Might be blocked or broken"}
+                          </div>
+                        </div>
                       </motion.label>
                     );
                   })}
-                  {languageResources.length > 0 && (
+                  {inputFields.languageResources.length > 0 && (
                     <FormError message={errors.languageResources} />
                   )}
                 </motion.div>
