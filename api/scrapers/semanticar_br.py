903b7936d12dba9106326af621ab5695d8551443
Taylor Barmak <37629992+TBarmak@users.noreply.github.com>
2025-09-05 18:30:46 -0400

    fix: update semanticar with new structure
    Semanticar started failing because the object key was updated from 'sentences' to 'initialSentences'. I also realized that Semanticar provided the definitions readily available, so I added the logic to fetch those as well.

--- api/scrapers/semanticar_br.py
+++ api/scrapers/semanticar_br.py
@@ -8,13 +8,15 @@ def create_url(word):
     return f"https://www.semanticar.com.br/{quote(word)}"
 
 
-def get_sentences(word, soup):
-    sentence_data = soup.find("script", {"id": "__NEXT_DATA__"}).text
-    sentence_objects = json.loads(sentence_data)[
-        "props"]["pageProps"]["sentences"]
-    sentences = [{"targetExampleSentences": [word.join(sent_obj["sentence"])]}
+def get_sentences_and_definitions(word, soup):
+    next_data = soup.find("script", {"id": "__NEXT_DATA__"}).text
+    sentence_objects = json.loads(next_data)[
+        "props"]["pageProps"]["initialSentences"]
+    parsed_sentences = [{"targetExampleSentences": [sent_obj["before"] + sent_obj["match"] + sent_obj["after"]]}
                  for sent_obj in sentence_objects]
-    return sentences
+    definitions = json.loads(next_data)["props"]["pageProps"]["definitions"]
+    parsed_definitions = [{"definition": definition} for definition in definitions]
+    return parsed_sentences + parsed_definitions
 
 
 def scrape_semanticar(word):
@@ -22,7 +24,7 @@ def scrape_semanticar(word):
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
-        sentence_objects = get_sentences(word, soup)
-        return sentence_objects, url, response.status_code
+        scraped_data = get_sentences_and_definitions(word, soup)
+        return scraped_data, url, response.status_code
     else:
         return [], url, response.status_code
